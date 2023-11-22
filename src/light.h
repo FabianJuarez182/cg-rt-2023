1d71cf759989e73987264809cca6ea0654baa349
Fabian Juarez <83832041+FabianJuarez182@users.noreply.github.com>
2023-11-21 18:05:37 -0600

    Initial commit

---
+++ src/light.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <glm/glm.hpp>
+#include "color.h"
+
+struct Light {
+  glm::vec3 position;
+  float intensity;
+  Color color;
+      Light(glm::vec3 position, float intensity, Color color) 
+        : position(position), intensity(intensity), color(color) {}
+};
